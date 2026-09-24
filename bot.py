a0ccc91cf6dd4db5e7e70828f1411b9e99ab386c
cakiya <43363058+cakiya@users.noreply.github.com>
2026-09-23 20:55:38 -0400

    logging print fix

--- bot.py
+++ bot.py
@@ -187,7 +187,7 @@ async def generate_bot_reply(channel: discord.abc.Messageable, author: discord.U
             "dry_penalty_last_n": 1024
         }
     )
-    print("=== RETURNING LLM RESPONSE ===")
+    print("\n\n=== RETURNING LLM RESPONSE ===")
     print(response)
     
     raw_reply = response.choices[0].message.content
